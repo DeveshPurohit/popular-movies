078e79ac72b3423574d5333a397a8a9d33a4350a
DeveshPurohit <100789752+DeveshPurohit@users.noreply.github.com>
2023-04-14 15:51:45 +0530

    Update page.jsx

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -11,7 +11,7 @@ const MovieDetails = async ({params}) => {
         <h3 className="m-2" >{res.runtime} min</h3>
         <h3 className="text-blue-500 m-2">{res.release_date}</h3>
         <h3 className="bg-green-500 inline-block rounded p-1 m-2">{res.status}</h3>
-        <Image className="m-2" src={'https://image.tmdb.org/t/p/original'+res.poster_path} height={250} width={300}/>
+        <Image className="my-2" src={'https://image.tmdb.org/t/p/original'+res.poster_path} height={400} width={400} priority/>
         <p className="m-2">{res.overview}</p>
     </div>
   )
